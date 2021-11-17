4582c08698f32eeb094ae925ab5ef396c13ec558
xdiaym <matthew.nekirov@gmail.com>
2021-11-17 21:55:00 +0300

    refactor(glob): setup project file structure

--- src/DBeditor.py
+++ src/DBeditor.py
@@ -2,37 +2,51 @@ from PyQt5 import QtCore, QtWidgets
 from sys import exit, argv
 import sqlite3
 
+
 class DBeditor(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi()
         self.setGeometry(200, 200, 770, 480)
-        self.setWindowTitle('Редактор базы данных')
+        self.setWindowTitle("Редактор базы данных")
         self.openDB.triggered.connect(self.openDBfunc)
-        # self.commitDB.triggered.connect(self.commitDBfunc)   
+        # self.commitDB.triggered.connect(self.commitDBfunc)
 
     def openDBfunc(self):
-        fname = QtWidgets.QFileDialog.getOpenFileName(self.centralwidget, 'Выберите базу данных', '', 
-        'Все базы данных SQLite (*.db *.sdb *.sqlite *.db3 *.s3db *.sqlite3 *.sl3)')
+        fname = QtWidgets.QFileDialog.getOpenFileName(
+            self.centralwidget,
+            "Выберите базу данных",
+            "",
+            "Все базы данных SQLite (*.db *.sdb *.sqlite *.db3 *.s3db *.sqlite3 *.sl3)",
+        )
         if fname[0]:
             self.con = sqlite3.connect(fname[0])
             self.cur = self.con.cursor()
-            self.tables = self.cur.execute("""SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite%'""").fetchall()
-            self.tableMenu = QtWidgets.QMenu('Таблица', self.menubar)
+            self.tables = self.cur.execute(
+                """SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite%'"""
+            ).fetchall()
+            self.tableMenu = QtWidgets.QMenu("Таблица", self.menubar)
             self.tablesActionGroup = QtWidgets.QActionGroup(self.menubar)
             for tableInd in range(len(self.tables)):
-                action = QtWidgets.QAction(self.tables[tableInd][0], self.menubar, checkable=True, checked=tableInd==0)
+                action = QtWidgets.QAction(
+                    self.tables[tableInd][0],
+                    self.menubar,
+                    checkable=True,
+                    checked=tableInd == 0,
+                )
                 self.tablesActionGroup.addAction(action)
                 self.tableMenu.addAction(action)
             self.menubar.addAction(self.tableMenu.menuAction())
             self.tablesActionGroup.triggered.connect(self.initTable)
             self.initTable(self.tables[0][0])
-            self.setWindowTitle(f'Редактор базы данных - {fname[0][fname[0].rfind("/") + 1:]}')
+            self.setWindowTitle(
+                f'Редактор базы данных - {fname[0][fname[0].rfind("/") + 1:]}'
+            )
             # self.tableWidget.itemChanged.connect(self.editBDfunc)
-    
+
     # def commitDBfunc(self):
     #     self.con.commit()
-    
+
     # def editBDfunc(self, item):
     #     ЧЁТ НАМУТИТЬ
 
@@ -41,7 +55,7 @@ class DBeditor(QtWidgets.QMainWindow):
             table = action
         else:
             table = action.text()
-        data = self.cur.execute(f'SELECT * FROM {table}')
+        data = self.cur.execute(f"SELECT * FROM {table}")
         names = list(map(lambda x: x[0], data.description))
         data = data.fetchall()
         self.tableWidget.setColumnCount(len(names))
@@ -49,7 +63,9 @@ class DBeditor(QtWidgets.QMainWindow):
         self.tableWidget.setRowCount(len(data))
         for row in range(len(data)):
             for col in range(len(names)):
-                self.tableWidget.setItem(row, col, QtWidgets.QTableWidgetItem(str(data[row][col])))
+                self.tableWidget.setItem(
+                    row, col, QtWidgets.QTableWidgetItem(str(data[row][col]))
+                )
         header = self.tableWidget.horizontalHeader()
         for col in range(len(names)):
             header.setSectionResizeMode(col, QtWidgets.QHeaderView.Stretch)
@@ -59,14 +75,18 @@ class DBeditor(QtWidgets.QMainWindow):
         self.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(self)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 770, 21))
-        self.fileMenu = QtWidgets.QMenu('Файл', self.menubar)
-        self.structureMenu = QtWidgets.QMenu('Структура', self.menubar)
+        self.fileMenu = QtWidgets.QMenu("Файл", self.menubar)
+        self.structureMenu = QtWidgets.QMenu("Структура", self.menubar)
         self.setMenuBar(self.menubar)
-        self.openDB = QtWidgets.QAction('Открыть', self.fileMenu)
-        self.createDB = QtWidgets.QAction('Создать', self.fileMenu)
-        self.createTable = QtWidgets.QAction('Создать таблицу', self.structureMenu)
-        self.commitDB = QtWidgets.QAction('Сохранить', self.fileMenu)
-        self.createColumn = QtWidgets.QAction('Создать столбец', self.structureMenu)
+        self.openDB = QtWidgets.QAction("Открыть", self.fileMenu)
+        self.createDB = QtWidgets.QAction("Создать", self.fileMenu)
+        self.createTable = QtWidgets.QAction(
+            "Создать таблицу", self.structureMenu
+        )
+        self.commitDB = QtWidgets.QAction("Сохранить", self.fileMenu)
+        self.createColumn = QtWidgets.QAction(
+            "Создать столбец", self.structureMenu
+        )
         self.fileMenu.addAction(self.openDB)
         self.fileMenu.addAction(self.createDB)
         self.fileMenu.addAction(self.commitDB)
@@ -81,7 +101,7 @@ class DBeditor(QtWidgets.QMainWindow):
         self.gridLayout.addWidget(self.status, 1, 0, 1, 1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QtWidgets.QApplication(argv)
     ex = DBeditor()
     ex.show()
