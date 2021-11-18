46f7640f9e8e29ddf9099c3ee6ec1cb9806fbf48
BazaroZero <finatov05@gmail.com>
2021-11-18 17:36:07 +0300

    Added ability: To change the items in the table, To select the directory in which the database will be created

--- DBeditor.py
+++ DBeditor.py
@@ -9,7 +9,8 @@ class DBeditor(QtWidgets.QMainWindow):
         self.setGeometry(200, 200, 770, 480)
         self.setWindowTitle('Редактор базы данных')
         self.openDB.triggered.connect(self.openDBfunc)
-        # self.commitDB.triggered.connect(self.commitDBfunc)   
+        self.commitDB.triggered.connect(self.commitDBfunc)
+        self.createDB.triggered.connect(self.createDBfunc)
 
     def openDBfunc(self):
         fname = QtWidgets.QFileDialog.getOpenFileName(self.centralwidget, 'Выберите базу данных', '', 
@@ -28,31 +29,56 @@ class DBeditor(QtWidgets.QMainWindow):
             self.tablesActionGroup.triggered.connect(self.initTable)
             self.initTable(self.tables[0][0])
             self.setWindowTitle(f'Редактор базы данных - {fname[0][fname[0].rfind("/") + 1:]}')
-            # self.tableWidget.itemChanged.connect(self.editBDfunc)
+            self.tableWidget.itemDoubleClicked.connect(self.saveItem)
+            self.tableWidget.itemChanged.connect(self.editBDfunc)
     
-    # def commitDBfunc(self):
-    #     self.con.commit()
+    def createDBfunc(self):
+        fname = QtWidgets.QFileDialog.getSaveFileName(self.centralwidget, 'Создайте базу данных', '', 'Все базы данных SQLite (*.db *.sdb *.sqlite *.db3 *.s3db *.sqlite3 *.sl3)')
+        self.con = sqlite3.connect(fname[0])
+        self.cur = self.con.cursor()
+        self.setWindowTitle(f'Редактор базы данных - {fname[0][fname[0].rfind("/") + 1:]}')
+
+    def commitDBfunc(self):
+        self.con.commit()
     
-    # def editBDfunc(self, item):
-    #     ЧЁТ НАМУТИТЬ
+    def saveItem(self, item):
+        self.prevItem = item.text()
+    
+    def editBDfunc(self, item):
+        try:
+            self.cur.execute(f"""UPDATE {self.chosenTable} SET {self.names[item.column()]} = ?
+                                    WHERE {self.names[self.primeKeyTables[0]]} = {self.tableWidget.item(item.row(), self.primeKeyTables[0]).text()}""", (item.text(),))
+        except sqlite3.Error as error:
+            self.tableWidget.setItem(self.prevItem.row(), self.prevItem.column(), self.prevItem)
+            self.status.setText(error)
 
     def initTable(self, action):
+        self.tableWidget.blockSignals(True)
         if isinstance(action, str):
-            table = action
+            self.chosenTable = action
         else:
-            table = action.text()
-        data = self.cur.execute(f'SELECT * FROM {table}')
-        names = list(map(lambda x: x[0], data.description))
+            self.chosenTable = action.text()
+        data = self.cur.execute(f'SELECT * FROM {self.chosenTable}')
+        self.names = list(map(lambda x: x[0], data.description))
         data = data.fetchall()
-        self.tableWidget.setColumnCount(len(names))
-        self.tableWidget.setHorizontalHeaderLabels(names)
+        self.primeKeyTables = [self.names.index(col) for col in self.cur.execute(f"""SELECT name 
+                                FROM pragma_table_info('{self.chosenTable}') WHERE pk = 1""").fetchall()[0]]
+        self.tableWidget.setColumnCount(len(self.names))
+        self.tableWidget.setHorizontalHeaderLabels(self.names)
         self.tableWidget.setRowCount(len(data))
         for row in range(len(data)):
-            for col in range(len(names)):
-                self.tableWidget.setItem(row, col, QtWidgets.QTableWidgetItem(str(data[row][col])))
+            for col in range(len(self.names)):
+                if col in self.primeKeyTables:
+                    item = QtWidgets.QTableWidgetItem(str(data[row][col]))
+                    item.setFlags(item.flags() ^ QtCore.Qt.ItemIsEditable)
+                    self.tableWidget.setItem(row, col, item)
+                else:
+                    self.tableWidget.setItem(row, col, QtWidgets.QTableWidgetItem(str(data[row][col])))
         header = self.tableWidget.horizontalHeader()
-        for col in range(len(names)):
+        for col in range(len(self.names)):
             header.setSectionResizeMode(col, QtWidgets.QHeaderView.Stretch)
+        self.tableWidget.blockSignals(False)
+        
 
     def setupUi(self):
         self.centralwidget = QtWidgets.QWidget(self)
