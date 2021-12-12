434282972bbb0c44189937381d3be5219308b537
xDiaym <matthew.nekirov@gmail.com>
2021-12-13 01:49:29 +0300

    test(tests): add tests for table builder

---
+++ tests/test_builder_group.py
@@ -0,0 +1,51 @@
+import pytest
+from sqlalchemy import Column, Integer
+
+from src.database import Database
+from src.table_builder import BuilderGroup
+
+
+@pytest.fixture
+def group(database: Database) -> BuilderGroup:
+    g = BuilderGroup(database.engine)
+    g.start_building("example")  # FIXME: fixture based on testing code
+    c = Column("id", Integer, primary_key=True, autoincrement=True)
+    g["example"].add_column(c)
+    return g
+
+
+@pytest.mark.parametrize(
+    "table_name, expected",
+    [("example", True), ("Lorem", False)]
+ )
+def test_builder_group_contains(
+    group: BuilderGroup, table_name: str, expected: bool
+) -> None:
+    assert (table_name in group) == expected
+
+
+def test_builder_group_getitem(group: BuilderGroup) -> None:
+    assert group["example"] == group._builders["example"]
+    with pytest.raises(KeyError):
+        _ = group["item"]
+
+
+# FIXME: depends on contains
+def test_builder_group_delitem(group: BuilderGroup) -> None:
+    del group["example"]
+    assert "example" not in group
+
+
+# FIXME: depends on contains
+def test_builder_group_start_building(group: BuilderGroup) -> None:
+    assert "new_table" not in group
+    group.start_building("new_table")
+    assert "new_table" in group
+    assert list(group["new_table"]) == []
+
+
+def test_builder_group_create_table(
+    group: BuilderGroup, database: Database
+) -> None:
+    group.create_table("example", database.metadata)
+    assert database.get_tables() == ["first", "second", "example"]
--- tests/test_table_builder.py
+++ tests/test_table_builder.py
@@ -1,6 +1,16 @@
+import pytest
 from sqlalchemy import Integer, String, Column
 
+from src.database import Database
 from src.table_builder import TableBuilder
+
+
+@pytest.fixture
+def builder() -> TableBuilder:
+    tb = TableBuilder("example")
+    for index in range(3):
+        tb.add_column(Column(f"c{index}", String(32)))
+    return tb
 
 
 def test_table_builder_with_no_columns() -> None:
@@ -14,3 +24,27 @@ def test_table_builder_with_columns() -> None:
     tb.add_column(Column("str", String(32)))
     columns = tb.build_table().columns.values()
     assert list(map(str, columns)) == ["example.id", "example.str"]
+
+
+# TODO: depend on get_tables
+def test_table_builder_with_meta(
+    builder: TableBuilder, database: Database
+) -> None:
+    meta = database.metadata
+    builder.build_table(meta)
+    assert database.get_tables() == ["first", "second", "example"]
+
+
+def test_table_builder_add_column_exception() -> None:
+    tb = TableBuilder("example")
+    tb.add_column(Column("str", String(32)))
+    with pytest.raises(ValueError):
+        tb.add_column(Column("str", String(32)))
+
+
+def test_table_builder_len(builder: TableBuilder) -> None:
+    assert len(builder) == 3
+
+
+def test_table_builder_iter(builder: TableBuilder) -> None:
+    assert list(builder) == ["c0", "c1", "c2"]
